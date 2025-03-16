15ada1ebcd040e0e522fcb709a5f3632ce2af6b5
Yuliia Shevchuk <shevchuuchkaaa@gmail.com>
2025-03-16 14:43:35 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('node:fs');
+
+function copy() {
+  const [sourceFile, destinationFile] = process.argv.slice(2);
+
+  if (process.argv.length !== 4) {
+    console.error('You need provide 4 arg');
+
+    return;
+  }
+
+  fs.copyFile(sourceFile, destinationFile, (error) => {
+    console.error(error);
+  });
+}
+
+copy();
